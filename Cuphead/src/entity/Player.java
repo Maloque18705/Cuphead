0dcba9ea3b918760bd96ff5e93a63978dbf8e9f1
Maloque18705 <maloque852@gmail.com>
2025-02-08 15:54:26 +0700

    Update New Objects, Collision failure caught

--- Cuphead/src/entity/Player.java
+++ Cuphead/src/entity/Player.java
@@ -39,8 +39,8 @@ public class Player extends Entity{
         getPlayerImage();
     }
     public void setDefaultValues() {
-        worldX = gp.tileSize * 23;
-        worldY = gp.tileSize * 21;
+        worldX = gp.tileSize * 10;
+        worldY = gp.tileSize * 15;
         speed = 4;
         direction = "down";
     }
--- Cuphead/src/main/GamePanel.java
+++ Cuphead/src/main/GamePanel.java
@@ -3,6 +3,7 @@ package main;
 import javax.swing.JPanel;
 
 import entity.Player;
+import main.object.SuperObject;
 import tile.TileManager;
 
 // import javax.swing.plaf.basic.BasicComboBoxUI.KeyHandler;
@@ -27,8 +28,8 @@ public class GamePanel extends JPanel implements Runnable {
     // WORLD SETTINGS
     public final int maxWorldCol = 50;
     public final int maxWorldRow = 50;
-    public final int worldWidth = tileSize * maxScreenCol;
-    public final int worldHeight = tileSize * maxScreenRow;
+    public final int worldWidth = tileSize * maxWorldCol; // Chỗ này từng viết là maxScreenCol
+    public final int worldHeight = tileSize * maxWorldRow;// Chỗ này từng viết là maxScreenRow
 
     // FPS
     int FPS = 60;
@@ -37,7 +38,10 @@ public class GamePanel extends JPanel implements Runnable {
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     public CollisionChecker cChecker = new CollisionChecker(this);
+    public AssetSetter aSetter = new AssetSetter(this);
     public Player player = new Player(this,keyH);
+    public SuperObject obj[] = new SuperObject[10];
+    
 
     // Set player's default position
     int playerX = 100;
@@ -51,6 +55,11 @@ public class GamePanel extends JPanel implements Runnable {
         this.setDoubleBuffered(true);
         this.addKeyListener(keyH);
         this.setFocusable(true);
+    }
+
+    public void setupGame() {
+        
+        aSetter.setObject();
     }
 
     public void startGameThread() {
@@ -131,8 +140,19 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D)g;
 
+        // TILE
         tileM.draw(g2);
 
+        // OBJECT
+        for(int i = 0; i < obj.length; i++) {
+            if(obj[i] != null) {
+                obj[i].draw(g2, this);
+            }
+        }
+
+        tileM.draw(g2);
+        
+        // PLAYER
         player.draw(g2);
 
         g2.dispose();
